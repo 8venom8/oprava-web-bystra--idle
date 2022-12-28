ff47596ba6a1494debcb2951eeb6ec0a120463d8
Vendula Stravová <venom111@email.cz>
2022-12-28 13:19:32 +0100

    oprava

---
+++ .eleventy.js
@@ -0,0 +1,23 @@
+module.exports = function(eleventyConfig) {
+    // Výchozí výstupní složka je: _site
+    // Zkopírovat images/ do _site/images
+    eleventyConfig.addPassthroughCopy("obrazky");
+    // Zkopírovat css/ to _site/css/
+    eleventyConfig.addPassthroughCopy("style.css");
+    eleventyConfig.addPassthroughCopy("contact.css");
+    eleventyConfig.addPassthroughCopy("live.css");
+    eleventyConfig.addPassthroughCopy("order.css");
+    eleventyConfig.addPassthroughCopy("obrazky-live");
+    eleventyConfig.addPassthroughCopy("obrazky-contact");
+    eleventyConfig.addPassthroughCopy("obrazky-order");
+   };
+
+   return {
+    // možné formáty šablon
+    templateFormats: ["njk", "html", "md"],
+    // jako šablonovací jazyk zvolíme Nunjucks`
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+  }
+
